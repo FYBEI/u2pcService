518e88f9453f3f845c4a1730b8f5a48fd57c44e7
lwx <1109302358@qq.com>
2019-05-27 17:16:47 +0800

    first commit

---
+++ src/main/java/com/uestc2h/u2pc/U2pcApplication.java
@@ -0,0 +1,13 @@
+package com.uestc2h.u2pc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2pcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(U2pcApplication.class, args);
+    }
+
+}
